e1723537901fe14e715985751c51d28b7fcdf98a
Zacharias Knudsen <z@chari.as>
2023-10-20 11:03:31 +0200

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>root</div>,
+  },
+  {
+    path: "/stores/:id",
+    element: <div>stors</div>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
